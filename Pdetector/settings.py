33b86960f9c02d5b7daac65de019e7e8ea377864
Cele56 <celestine.udoudo2992@gmail.com>
2024-07-12 15:31:18 +0100

    updates

--- Pdetector/settings.py
+++ Pdetector/settings.py
@@ -20,7 +20,7 @@ env = environ.Env(
 )
 
 ENVIRONMENT = env('ENVIRONMENT', default='production')
-ENVIRONMENT = 'development'
+ENVIRONMENT = 'production'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
