a0606cc6250f2ccbe0e925677c0a0861dddcea1d
Aniebiet Francis <102663124+Anie22@users.noreply.github.com>
2024-08-10 23:38:32 +0100

    Update

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -224,6 +224,8 @@ class ResetPasswordSerializer(serializers.Serializer):
         except Exception as e:
             raise serializers.ValidationError(f'Unable to send mail {str(e)}')
 
+        return attrs
+
 class UpdatePasswoedSerializer(serializers.Serializer):
     password = serializers.CharField(max_length=30, min_length=8, write_only=True)
     confirm_password = serializers.CharField(max_length=30, min_length=8, write_only=True)
